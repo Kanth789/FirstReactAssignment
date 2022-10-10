499c19675cc298cb3273322e9e05039250def303
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-10 18:07:46 +0530

    Commit Social-button

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -2,11 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+ 
+    const Button = (props) => {
+      const { ButtonClassName, ButtonText } = props;
+      return (
+        <button className={ButtonClassName}>
+          {ButtonText}
+        </button>
+      );
+    };
+    
+   const Element =() =>(
     <div className="App">
-      
+     <div className="text-conatiner"><h3>Social Buttons</h3></div>
+     <div className="Button-conatiner">
+      <Button ButtonClassName ="like-button" ButtonText="Like"></Button>
+      <Button ButtonClassName ="Commnet-button" ButtonText="Comment"></Button>
+      <Button ButtonClassName ="share-button" ButtonText="Share"></Button>
+     </div>
     </div>
-  );
+   )
+  return(<Element/>)
+  
 }
-
 export default App;
