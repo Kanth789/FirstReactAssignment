a3991ef57eb503393d2d8d38f27dd19435248704
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-10 16:06:55 +0530

    congrats card commit

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="Head-conatiner">
+        <h3>Congratulations</h3>
+      </div>
+      <div className="Second-conatiner">
+        <div className="img-div"><img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/></div>
+        <div className="name-of-person"><h3>Kiran Kanth</h3></div>
+        <div className="name-of-college"><h5>Lovely Professional University</h5></div>
+        <div className="watch-img"><img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/></div>
+      </div>
     </div>
   );
 }
