b55c52e02431fb490bba428688d55b3113815323
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-12 17:19:38 +0530

    Google search function with uniqueNo and content

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import GoogleSearch from './components/GoogleSearch/GoogleSearch';
+
 const GoogleList = [
   {
     uniqueNo:1,
@@ -26,7 +27,7 @@ const GoogleList = [
   {
     uniqueNo:6,
     content:"Mast & Habour"
-  }
+  },
 ]
  class App extends Component{
   state = {
@@ -37,22 +38,34 @@ const GoogleList = [
       searchInput : event.target.value
     })
   }
+  onHighlightApp = uniqueNo =>{
+    const filterData = GoogleList.find(eachItem => eachItem.uniqueNo === uniqueNo)
+    console.log(filterData)
+      this.setState({searchInput:filterData.content})
+  }
+  // onHighlightApp = content => {
+  //   const {searchInput} = this.state
+  //   this.setState({searchInput:content})
+  // }
   render(){
     const {searchInput} = this.state
-    const searchResult = GoogleList.filter((eachItem)=>eachItem.content.toLowerCase().includes(searchInput.toLowerCase()),)
+    const searchResult = GoogleList.filter((eachItem) => eachItem.content.toLowerCase().includes(searchInput.toLowerCase()))
     return(
       <div className='main-conatiner'>
         <div className='image'>
         <img src='https://assets.ccbp.in/frontend/react-js/google-logo.png '></img>
         </div>
+        <div className='main-box'>
         <div className='searchInput'>
-          <input type="search" onChange = {this.onChangetext} ></input>
+        <img src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png "></img>
+          <input type="search" value={searchInput} onChange = {this.onChange} placeholder="Enter here ..."></input>
         </div>
         <div className="box">
-          {searchResult.map(eachItem =>
-            (<GoogleSearch GoogleList={eachItem} key={eachItem.uniqueNo}/>
+          { searchResult.map(eachItem =>
+            (<GoogleSearch  onHighlightApp={this.onHighlightApp}GoogleList={eachItem} key={eachItem.uniqueNo}/>
           ))}
         </div>
+      </div>
       </div>
     )
   }
--- react-ssignments/src/components/GoogleSearch/GoogleSearch.js
+++ react-ssignments/src/components/GoogleSearch/GoogleSearch.js
@@ -1,10 +1,17 @@
 const GoogleSearch = (props) =>{
-    const{GoogleList} = props
+    const {GoogleList,onHighlightApp} = props
+    const {uniqueNo,content} = GoogleList
+    
+    const onHighlight = () =>{
+        onHighlightApp(uniqueNo)
+        console.log(uniqueNo)
+    }
     return(
         
             <div className="div1">
-                <p>{GoogleList.content}</p>
-                <img src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"></img>
+                
+                <p>{content}</p>
+                <img src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png" onClick={onHighlight}></img>
             </div>
         
     )
