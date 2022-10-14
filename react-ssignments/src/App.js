1f1fe3e6ae9a1c70732f8ddf5332149b55a1a9aa
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-14 11:01:57 +0530

    App store css styling completed

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -6,110 +6,98 @@ import DisplayApp from './components/DisplayApp';
 const AppStore = [
   {
     uniqueNo:1,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgUrl:"https://img.icons8.com/fluency/344/facebook-new.png",
     name:"Facebook",
     caterogyId:"social"
   },
   {
     uniqueNo:2,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgUrl:"https://img.icons8.com/color/344/instagram-new--v1.png",
     name:"Instagram",
     caterogyId:"social"
   },
   {
     uniqueNo:3,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgUrl:"https://img.icons8.com/color/344/youtube-play.png",
     name:"Youtube",
     caterogyId:"social"
   },
   {
     uniqueNo:4,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgUrl:"https://img.icons8.com/3d-fluency/344/tiktok.png",
     name:"TikTok",
     caterogyId:"social"
   },
   {
     uniqueNo:5,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgUrl:"https://img.icons8.com/external-tal-revivo-color-tal-revivo/344/external-resso-application-for-android-and-ios-devices-for-music-listening-and-streaming-music-color-tal-revivo.png",
     name:"Resso",
     caterogyId:"social"
   },
   {
     uniqueNo:6,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Facebook",
+    imgUrl:"https://img.icons8.com/color/344/subway-surfers.png",
+    name:"Subway-surfers",
     caterogyId:"Games"
   },
   {
     uniqueNo:7,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Instagram",
+    imgUrl:"https://img.icons8.com/external-those-icons-lineal-color-those-icons/344/external-angry-birds-video-games-those-icons-lineal-color-those-icons.png",
+    name:"Angry-birds",
     caterogyId:"Games"
   },
   {
     uniqueNo:8,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Youtube",
+    imgUrl:"https://img.icons8.com/bubbles/344/temple-run.png",
+    name:"Temple-run",
     caterogyId:"Games"
   },
   {
     uniqueNo:9,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"TikTok",
+    imgUrl:"https://img.icons8.com/cute-clipart/344/hill-descent-control.png",
+    name:"Hill-Climb",
     caterogyId:"Games"
   },
   {
     uniqueNo:10,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Resso",
+    imgUrl:"https://img.icons8.com/color/344/jerry.png",
+    name:"jerry",
     caterogyId:"Games"
   },
   {
     uniqueNo:11,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Facebook",
+    imgUrl:"https://img.icons8.com/cute-clipart/344/inshorts.png",
+    name:"Inshorts",
     caterogyId:"News"
   },
   {
     uniqueNo:12,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Instagram",
+    imgUrl:"https://img.icons8.com/bubbles/344/news.png",
+    name:"Google News",
     caterogyId:"News"
   },
   {
     uniqueNo:13,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Youtube",
+    imgUrl:"https://img.icons8.com/ios-filled/344/bbc-logo.png",
+    name:"BBC News",
     caterogyId:"News"
   },
   {
     uniqueNo:14,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"TikTok",
-    caterogyId:"News"
+    imgUrl:"https://img.icons8.com/doodle/344/swiggy.png",
+    name:"Swiggy",
+    caterogyId:"Food"
   },
   {
     uniqueNo:15,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Resso",
-    caterogyId:"News"
+    imgUrl:"https://img.icons8.com/ios-filled/344/zomato-logo.png",
+    name:"Zomato",
+    caterogyId:"Food"
   },
   {
     uniqueNo:16,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Youtube",
-    caterogyId:"Food"
-  },
-  {
-    uniqueNo:17,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"TikTok",
-    caterogyId:"Food"
-  },
-  {
-    uniqueNo:18,
-    imgUrl:"https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name:"Resso",
+    imgUrl:"https://img.icons8.com/bubbles/344/zepeto.png",
+    name:"Zepto",
     caterogyId:"Food"
   }
 
@@ -168,6 +156,10 @@ class App extends Component
         </div>
         <div className='searchInput'>
           <input type="search" onChange={this.onChange}></input>
+          
+        </div>
+        <div className='img-icon'>
+        <img src='https://assets.ccbp.in/frontend/react-js/app-store/app-store-search-img.png'></img>
         </div>
         <div className='tabs'>
           {TabsList.map(eachItem=>(<Tabs OnclickedTabApp={this.OnStateUpdate} TabsList={eachItem} key={eachItem.uniqueNo}/>))}
