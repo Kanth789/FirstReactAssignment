5546dd48f47f82fa1b00cbc00de9ad3fb53ba138
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-14 12:53:31 +0530

    Coin-Toss- App Functions and styling

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -1,12 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const TabsList = [
+  {
+    uniqueNo:1,
+    Name:"Total",
+
+    
+  },
+  {
+    uniqueNo:2,
+    Name:"Heads",
+    imgUrl:"https://i.ibb.co/bvdsr2w/Screenshot-from-2022-10-14-12-38-37.png"
+    
+  },
+  {
+    uniqueNo:3,
+    Name:"Tails",
+    imgUrl:"https://i.ibb.co/BtkV0dd/Screenshot-from-2022-10-14-12-37-35.png"
+    
+  }
+]
+
+let TossList = ["Heads","Tails"]
+
+class App extends Component{
+  state ={
+    stateUrl:"https://i.ibb.co/bvdsr2w/Screenshot-from-2022-10-14-12-38-37.png",
+    HeadsCount:0,
+    TailsCount:0
+  }
+  Onclicked = () =>{
+      let TossRandom = TossList[Math.ceil(Math.random() * TossList.length) - 1]
+
+    const ImgUpdate = TabsList.find((eachImage)=>eachImage.Name === TossRandom)
+    this.setState({stateUrl:ImgUpdate.imgUrl})
+    if(ImgUpdate.Name === "Heads"){
+    this.setState((prevState)=>({...prevState,HeadsCount:prevState.HeadsCount  + 1}))
+    }else{
+    this.setState((prevState)=>({...prevState,TailsCount:prevState.TailsCount  + 1}))
+  }
 }
-
+  render()
+  {
+    
+    const{stateUrl,HeadsCount,TailsCount} = this.state
+    return(
+      <div className='main-conatiner'>
+        <div className='main'>
+          <div className='header'>
+            <h3 className='heading'>Coin Toss Game</h3>
+          </div>
+          <div className='para'>
+            <p>Heads (or) Tails</p>
+          </div>
+          <div className='image-Conatiner'>
+            <img src={stateUrl}></img>
+          </div>
+          <div className='button-conatiner'>
+            <button onClick={this.Onclicked}>Toss Coin</button>
+          </div>
+          <div className='Tabs'>
+            <div className='tab1'>
+              <p>Total:{HeadsCount + TailsCount}</p>
+            </div>
+            <div className='tab1'>
+              <p>Heads:{HeadsCount}</p>
+            </div>
+            <div className='tab1'>
+              <p>Tails:{TailsCount}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
---
+++ react-ssignments/src/components/CoinToss/CoinToss.js
@@ -0,0 +1,9 @@
+// const CoinToss = (props) =>{
+//     const {TabsList} = props
+//     const {uniqueNo,Name,imgUrl} = TabsList
+//     return(
+//         <div className="tabs">
+//             <div className={Name}></div>
+//         </div>
+//     )
+// }
