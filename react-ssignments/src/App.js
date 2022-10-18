0ea14676686285f1ca82b4b5754717021ed77fc7
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-18 12:34:06 +0530

    clock compoennt life cycle

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { Component } from 'react';
+import Clock from './components/clock';
+class App extends Component{
+  state = {
+    showClock : false,
+  }
+  onToggleClock = () =>{
+    this.setState(prevState =>{
+      const{showClock} = prevState
+      return{
+        showClock:!showClock,
+      }
+    })
+  }
+  render(){
+    const{showClock} = this.state
+    return(
+      <div className='app-conatiner'>
+        <button type='button' onClick={this.onToggleClock}className='toggle-buton'>{showClock ? 'Hide Clock':'Show Clock'}</button>
+      {showClock && <Clock/>}
+      </div>
+    )
+  }
 }
-
 export default App;
