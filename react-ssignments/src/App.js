ee2ddf1b1af4e74ade2e83816eee465e08a2d5d9
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-19 12:28:47 +0530

    Password-manager styling and functions

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -1,12 +1,105 @@
+/* eslint-disable jsx-a11y/alt-text */
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import Password from './components/Password';
+const PasswordList = []
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  state={
+     statePasswordList : PasswordList,
+     website:'',
+     username:'',
+     password:'',
+     isShowButton: false
+  }
+  Onchangewebsite = (event)=>{
+     this.setState({website:event.target.value})
+     console.log(event.target.value)
+  }
+  Onchangeusername = (event)=>{
+     this.setState({username:event.target.value})
+     console.log(event.target.value)
+  }
+  OnchangePassword = (event) =>{
+    this.setState({password:event.target.value})
+    console.log(event.target.value)
+  }
+  onAddPassword = (event)=>{
+    event.preventDefault() 
+    const {website,username,password} = this.state
+    console.log(this.state)
+    const newPassword = {
+      uniqueNo: uuidv4(),
+      website,
+      username,
+      password,
+      isShowButton:false
+    }
+    this.setState(prevState=>({statePasswordList:[...prevState.statePasswordList,newPassword],
+      website:'',
+      username:'',
+      password:''
+    }))
+  }
+  Onchecked = () =>{
+      const{isShowButton,password,statePasswordList} = this.state
+      this.setState(prevState=>({isShowButton:!prevState.isShowButton}))
+      // eslint-disable-next-line no-undef
+     
       
-    </div>
-  );
+  }
+  onDeleteApp = (uniqueNo) =>{
+    const {statePasswordList} = this.state
+    const filteredData = statePasswordList.filter(eachItem =>eachItem.uniqueNo !== uniqueNo)
+    this.setState({statePasswordList:filteredData})
+  }
+  render(){
+    const{statePasswordList,website,username,password,isShowButton} = this.state
+    return(
+      <div className='main-conatiner'>
+        <div className='main'>
+          <div className='logo'>
+            <img src='https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png'/>
+          </div>
+          <div className='first-Conatiner'>
+            <div className='form-conatiner'>
+              <form  onSubmit={this.onAddPassword}>
+                <label>Add New Password</label>
+                <input value={website} type="text"  onChange={this.Onchangewebsite} placeholder='Enter the Website...'></input>
+                <input value={username} type="text" onChange={this.Onchangeusername} placeholder='Enter the UserName...'></input> 
+                <input  value={password} type="password" onChange={this.OnchangePassword} placeholder='Enter the Website..'></input>
+                <button >Add</button>
+              </form>
+            </div>
+            <div className='first-image'>
+              <img src='https://assets.ccbp.in/frontend/react-js/password-manager-sm-img.png'/>
+            </div>
+          </div>
+          <div className='second-conatiner'>
+            <div className='second-header'>
+              <div className='header'>
+                <h3>Your Password<span></span></h3>
+              </div>
+              <div className='header-input'>
+              <input type="text"></input>
+              </div>
+              
+            </div>
+            <div className='checkBox'>
+            <input type="checkbox" onClick={this.Onchecked} value="password"/>
+            <label> Show Password</label>
+            </div>
+            <div className='passwords-conatiner'>
+              {statePasswordList.map(eachItem=>(<Password key={eachItem.uniqueNo} PropPassword={eachItem} OncheckedButton={this.isShowButton} onDeleteApp = {this.onDeleteApp}/>))}
+              </div>
+             
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ react-ssignments/src/components/Password.js
@@ -0,0 +1,28 @@
+const Password = (props) =>{
+    const{PropPassword,isShowButton,onDeleteApp} = props
+    const{uniqueNo,website,username,password} = PropPassword
+    const passwordShow = isShowButton ? password : "*".repeat(password.length)
+    const OnDelete = () =>{
+        onDeleteApp(uniqueNo)
+    }
+    return(
+        <div className="div1">
+            <div className="logo">
+                <div className="logo-circle"></div>
+            </div>
+            <div className="div-content">
+                <div className="user-name">
+                    <h4>{website}</h4>
+                    <h5>{username}</h5>
+                    <p>{passwordShow}</p>
+                    
+                </div>
+            </div>
+            <div className='delete'>
+                <img src='https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png ' onClick={OnDelete}/>
+              </div>
+        </div>
+    )
+}
+
+export default Password
