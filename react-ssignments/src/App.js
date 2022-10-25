737ce8f04f3e8c2d10e16442f5ed7aa5641c22ea
Kiran Kanth <kanthkiran020@gmail.com>
2022-10-25 12:05:31 +0530

    Routing Functions and styling done

--- react-ssignments/src/App.js
+++ react-ssignments/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
-function App() {
+import { Header } from './component/Header';
+import { Home } from './component/Home';
+import { About } from './component/About';
+import { Contact } from './component/Contact';
+import { NotFound } from './component/NotFound';
+
+ const App = () =>{
   return (
-    <div className="App">
-      
-    </div>
-  );
+  <BrowserRouter>
+  <Header/>
+    <Routes>
+      <Route  path="/about" element={<About/>}></Route>
+      <Route  path="/contact" element={<Contact/>}></Route>
+      <Route  path="/" element={<Home/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
+    </Routes>
+ </BrowserRouter>
+  )  
 }
-
-export default App;
+export default App
---
+++ react-ssignments/src/component/Home.js
@@ -0,0 +1,12 @@
+export const Home = () =>{
+    return(
+        <>
+    <div className="image">
+        <img src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png" alt="home"></img>
+    </div>
+    <div className="img-text">
+        Home
+    </div>
+    </>
+    )
+}
