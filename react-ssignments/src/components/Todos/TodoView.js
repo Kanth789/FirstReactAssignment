417a3dfbf886e53bf6d6085db62dcb480dfb4b4a
Kiran Kanth <kanthkiran020@gmail.com>
2022-11-22 15:11:08 +0530

    todoview i18n

--- react-ssignments/src/components/Todos/TodoView.js
+++ react-ssignments/src/components/Todos/TodoView.js
@@ -1,11 +1,11 @@
 import { observer } from "mobx-react-lite";
 import {makeObservable,autorun,observable, action,computed,reaction,toJS } from "mobx"
-
+import {useTranslation,Trans} from 'react-i18next'
 import UserStore from "../UserStore";
 const  TodoView = observer(({ todo }) =>{
   const  todoListStore  = UserStore;
   console.log(toJS(todo),"toodo")
-    
+  const {t} = useTranslation()  
   return (
     <>
     <li>
@@ -18,7 +18,7 @@ const  TodoView = observer(({ todo }) =>{
           checked={todo.isChecked}
         />
         <label>{todo.name}</label>
-        <button  onClick={() => todoListStore.deleteTodo(todo.id)}>Delete</button>
+        <button  onClick={() => todoListStore.deleteTodo(todo.id)}>{t('button-delete')}</button>
       </div>
       
     </li>
